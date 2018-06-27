ed2091932cf4cbede5151a71668c33bc89c82007
iamezell <iamezell@gmail.com>
2018-06-26 23:29:39 -0400

    adding test for input

--- src/states/Boot.js
+++ src/states/Boot.js
@@ -6,6 +6,12 @@ export default class extends Phaser.State {
     this.stage.backgroundColor = '#EDEEC9'
     this.fontsReady = false
     this.fontsLoaded = this.fontsLoaded.bind(this)
+    this.scale.scaleMode = Phaser.ScaleManager.SHOW_ALL;
+    this.scale.pageAlignHorizontally = true;
+    this.scale.pageAlignVertically = true;
+
+    this.physics.startSystem(Phaser.Physics.ARCADE);
+    this.physics.arcade.gravity.y = 1000
   }
 
   preload () {
@@ -18,7 +24,15 @@ export default class extends Phaser.State {
 
     let text = this.add.text(this.world.centerX, this.world.centerY, 'loading fonts', { font: '16px Arial', fill: '#dddddd', align: 'center' })
     text.anchor.setTo(0.5, 0.5)
+    this.load.image('ground', 'assets/images/ground.png');    
+    this.load.image('platform', 'assets/images/platform.png');    
+    this.load.image('goal', 'assets/images/gorilla3.png');    
+    this.load.image('arrowButton', 'assets/images/arrowButton.png');    
+    this.load.image('actionButton', 'assets/images/actionButton.png');    
+    this.load.image('barrel', 'assets/images/barrel.png');    
 
+    this.load.spritesheet('player', 'assets/images/player_spritesheet.png', 28, 30, 5, 1, 1);    
+    this.load.spritesheet('fire', 'assets/images/fire_spritesheet.png', 20, 21, 2, 1, 1);
     this.load.image('loaderBg', './assets/images/loader-bg.png')
     this.load.image('loaderBar', './assets/images/loader-bar.png')
   }
--- src/states/Game.js
+++ src/states/Game.js
@@ -6,10 +6,16 @@ import Hero from '../actors/Hero'
 import Kobold from '../actors/Kobold'
 
 export default class extends Phaser.State {
-  init () {}
+  init () {
+    this.cursors = this.game.input.keyboard.createCursorKeys();
+
+    this.RUNNING_SPEED = 180;
+    this.JUMPING_SPEED = 550;
+  }
   preload () {}
 
   create () {
+    
     const bannerText = 'Battle Demo'
     const roundText = 'Round'
     const roundNumberText = '0'
@@ -24,55 +30,76 @@ export default class extends Phaser.State {
     AttackBtn.anchor.set(0.5)
     AttackBtn.inputEnabled = true;
     AttackBtn.events.onInputDown.add(this.onAttack, this);
+    //  Register the keys.
+    this.leftKey = this.input.keyboard.addKey(69)
+    this.rightKey = this.input.keyboard.addKey(Phaser.Keyboard.RIGHT);
+    this.spaceKey = this.input.keyboard.addKey(Phaser.Keyboard.SPACEBAR);
+
     // banner styling
-    this.banner.font = 'Bangers'
-    this.banner.padding.set(10, 16)
-    this.banner.fontSize = 40
-    this.banner.fill = '#77BFA3'
-    this.banner.smoothed = false
-    this.banner.anchor.setTo(0.5)
+    // this.banner.font = 'Bangers'
+    // this.banner.padding.set(10, 16)
+    // this.banner.fontSize = 40
+    // this.banner.fill = '#77BFA3'
+    // this.banner.smoothed = false
+    // this.banner.anchor.setTo(0.5)
 
-    this.round.font = 'Bangers'
-    this.round.padding.set(10, 16)
-    this.round.fontSize = 40
-    this.round.fill = '#77BFA3'
-    this.round.smoothed = false
-    this.round.anchor.setTo(0.5)
+    // this.round.font = 'Bangers'
+    // this.round.padding.set(10, 16)
+    // this.round.fontSize = 40
+    // this.round.fill = '#77BFA3'
+    // this.round.smoothed = false
+    // this.round.anchor.setTo(0.5)
    
-    this.roundNumber.font = 'Bangers'
-    this.roundNumber.padding.set(10, 16)
-    this.roundNumber.fontSize = 40
-    this.roundNumber.fill = '#77BFA3'
-    this.roundNumber.smoothed = false
-    this.roundNumber.anchor.setTo(0.5)
+    // this.roundNumber.font = 'Bangers'
+    // this.roundNumber.padding.set(10, 16)
+    // this.roundNumber.fontSize = 40
+    // this.roundNumber.fill = '#77BFA3'
+    // this.roundNumber.smoothed = false
+    // this.roundNumber.anchor.setTo(0.5)
 
 
-    this.mushroom = new Mushroom({
-      game: this.game,
-      x: this.world.centerX,
-      y: this.world.centerY,
-      asset: 'mushroom'
-    })
+    // this.mushroom = new Mushroom({
+    //   game: this.game,
+    //   x: this.world.centerX,
+    //   y: this.world.centerY,
+    //   asset: 'mushroom'
+    // })
 
-    //this.game.add.existing(this.mushroom)
-    this.game.add.existing(myHero)
-    this.game.add.existing(theKobold)
+    this.ground = this.add.sprite(0, 500, 'ground');
+    this.game.physics.arcade.enable(this.ground);
+    this.ground.body.allowGravity = false;
+    this.ground.body.immovable = true;
+
+    var platform = this.add.sprite(0, 300, 'platform');
+    this.game.physics.arcade.enable(platform);
+    platform.body.allowGravity = false;
+    platform.body.immovable = true;
+    
+    //create player
+    this.player = this.add.sprite(100, 200, 'player', 3);
+    this.player.anchor.setTo(0.5);
+    this.player.animations.add('walking', [0, 1, 2, 1], 6, true);
+    this.game.physics.arcade.enable(this.player);
+
+    // this.game.add.existing(this.mushroom)
+    // this.game.add.existing(myHero)
+    // this.game.add.existing(theKobold)
     // place the fighters in a match
-    let match = []
-    match.push(myHero)
-    match.push(theKobold)
-    let matchArray = match.map((char)=>{
-      char.rollInititive()
-      console.log('the char named'+char.name+' has inititive', char.inititive)
-      return char
-    }).sort((a,b) => {
-      if(a.inititive > b.inititive)
-        return -1
-      if(a.inititive < b.inititive)
-        return 1
-      return 0
-      })
-   let attack = matchArray[0].attack(matchArray[1])
+    // let match = []
+    // match.push(myHero)
+    // match.push(theKobold)
+  //   let matchArray = match.map((char)=>{
+  //     char.rollInititive()
+  //     console.log('the char named'+char.name+' has inititive', char.inititive)
+  //     return char
+  //   }).sort((a,b) => {
+  //     if(a.inititive > b.inititive)
+  //       return -1
+  //     if(a.inititive < b.inititive)
+  //       return 1
+  //     return 0
+  //     })
+  //  let attack = matchArray[0].attack(matchArray[1])
   }
 
   onAttack () {
@@ -81,14 +108,36 @@ export default class extends Phaser.State {
 
   update = () => {
    // console.log('updating!!')
-   switch(this.gameState) {
-     case 'startRound':
-       this.gameState = 'endRound'
-       break;
-     case 'endRound':
-       console.log('should see this allot')
-       break;
-   }
+   this.physics.arcade.collide(this.player, this.ground);
+   this.physics.arcade.overlap(this.player, this.platform, this.landed);
+
+   this.player.body.velocity.x = 0;
+   if (game.input.keyboard.isDown(Phaser.Keyboard.LEFT))
+    {
+       console.log('left is down')
+    }
+    else if (game.input.keyboard.isDown(Phaser.Keyboard.RIGHT))
+    {
+      console.log('right is down')
+    }
+    else
+    {
+       
+      console.log('none is down')
+    }
+    
+  //  switch(this.gameState) {
+  //    case 'startRound':
+  //      this.gameState = 'endRound'
+  //      break;
+  //    case 'endRound':
+  //      console.log('should see this allot')
+  //      break;
+  //  }
+  }
+
+  landed = () => {
+    console.log('landed')
   }
 
   render () {
